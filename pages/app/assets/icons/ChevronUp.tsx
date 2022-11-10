75f7a3e8768584a9fb16c4aefb44f86df5465dd9
Malak Joseph <malak@wuilt.com>
2022-11-10 06:36:00 +0200

    interactive chevron

--- pages/app/assets/icons/ChevronUp.tsx
+++ pages/app/assets/icons/ChevronUp.tsx
@@ -1,11 +1,26 @@
-const ChevronUp = () => (
+enum RotateDirection {
+  Up = "rotate-0",
+  Right = "rotate-90",
+  Down = "rotate-180",
+  Left = "-rotate-90",
+}
+
+type Rotate = keyof typeof RotateDirection;
+
+const ChevronUp = ({
+  rotate = "Up",
+  small,
+}: {
+  rotate?: Rotate;
+  small?: boolean;
+}) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
-    strokeWidth={1.5}
+    strokeWidth={small ? 3 : 1.5}
     stroke="currentColor"
-    className="w-6 h-6"
+    className={`${small ? "w-2 h-2" : "w-4 h-4"} ${RotateDirection[rotate]}`}
   >
     <path
       strokeLinecap="round"
