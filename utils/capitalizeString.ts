9a0f81c7032e88fbe4d8e9162f142bc92b4165d0
Malak Joseph <malak@wuilt.com>
2022-11-11 02:57:37 +0200

    revamp some table header keys

--- utils/capitalizeString.ts
+++ utils/capitalizeString.ts
@@ -1,6 +1,15 @@
 export function capitalizeString(str: string) {
+  if (str === "creationTimestamp") {
+    return "Date : Time";
+  }
+
   const pascalStr = str[0].toUpperCase() + str.slice(1);
   const splittedStr = pascalStr.split(/(?=[A-Z])/);
+
+  if (splittedStr[1] === "Id") {
+    splittedStr[1] = "ID";
+  }
+
   const capitalizedStr = splittedStr.join(" ");
 
   return capitalizedStr;
